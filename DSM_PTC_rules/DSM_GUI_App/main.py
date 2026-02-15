5b82d7689747940a648236fbc5af2e232200561e
Ritesh Kumar Singh <riteshitkumar@gmail.com>
2026-02-16 02:35:21 +0530

    Add main.py for DSM GUI application entry point

---
+++ DSM_PTC_rules/DSM_GUI_App/main.py
@@ -0,0 +1,10 @@
+
+from gui import DSMApp
+import customtkinter as ctk
+
+if __name__ == "__main__":
+    ctk.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+    ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+    
+    app = DSMApp()
+    app.mainloop()
